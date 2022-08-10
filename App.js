7f1622938743548ba5bb783e7fdd7012cb7d2e62
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-08-10 17:59:01 -0300

    #

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import MainScreen from "./src/components/MainScreen/MainScreen";
+import SecondScreen from "./src/components/SecondScreen/SecondScreen";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={() => ({
+        headerBackTitle: null,
+        headerStyle: { backgroundColor: "#02ADAD" },
+        headerTitleStyle: {
+          color: "white",
+          flex: 1,
+          textAlign: "center",
+          fontSize: 16
+        },
+        headerTintColor: "white"
+      })}>
+        <Stack.Screen name="MainScreen" component={MainScreen} />
+        <Stack.Screen name="SecondScreen" component={SecondScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
